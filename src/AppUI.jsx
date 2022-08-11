811690788cd48204e8e06b5181d3785953fcb46a
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-08-11 14:06:31 -0600

    [ADD] Primer commit

---
+++ src/AppUI.jsx
@@ -0,0 +1,15 @@
+import './AppUI.css'
+
+function AppUI() {
+
+  return (
+    <div className="AppUI">
+      <h1>
+        Hola Mundo!
+        :D no funciona el don
+      </h1>
+    </div>
+  )
+}
+
+export default AppUI
